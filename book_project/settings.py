531987b85265cf09c254d80f4c4dd8f78116ac2d
rodrigues-i <rodrigues-i@outlook.com>
2024-12-04 20:39:34 -0300

    Disable authentication

--- book_project/settings.py
+++ book_project/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],  # Disable authentication for now
+    'DEFAULT_PERMISSION_CLASSES': [],     # Disable permission checks for now
+}
