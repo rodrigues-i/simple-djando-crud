2e37bcb1cbf70391eb9707c0e8970a3604df4a4c
rodrigues-i <rodrigues-i@outlook.com>
2024-12-04 20:33:04 -0300

    Create simple crud

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# List and Create
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Retrieve, Update, and Delete
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
